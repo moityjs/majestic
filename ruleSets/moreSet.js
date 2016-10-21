fbcac4b4680a0dcdb96d462f02b4faec874e74d4
Peet <peet@peet.me.uk>
2016-10-21 15:03:31 +0100

    turn on more/force-native-methods

--- ruleSets/moreSet.js
+++ ruleSets/moreSet.js
@@ -1,6 +1,6 @@
 module.exports = require('../utils/pluginRules').with('more', {
   'classbody-starts-with-newline': ['error', 'never'],
-  'force-native-methods': 'off',
+  'force-native-methods': 'warn',
   'no-duplicated-chains': 'error',
   'no-numeric-endings-for-variables': 'error',
   'no-then': 'off',
