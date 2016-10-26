36fdb09cfbd4d651d3b5932805c0c6c33154019a
Peet <peet@peet.me.uk>
2016-10-26 15:38:50 +0100

    index.js files are fine

--- ruleSets/filenamesSet.js
+++ ruleSets/filenamesSet.js
@@ -1,5 +1,5 @@
 module.exports = require('../utils/pluginRules').with('filenames', {
   'match-exported': 'error',
   'match-regex': ['error', '^[a-z_]+$', true],
-  'no-index': 'warn',
+  'no-index': 'off',
 });
