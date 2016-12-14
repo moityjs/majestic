4819b8e9cf61fa126841df7e4398e70952a2fef5
Peet <peet@peet.me.uk>
2016-12-14 14:51:13 +0000

    update packages, define new rules

--- ruleSets/moreSet.js
+++ ruleSets/moreSet.js
@@ -1,8 +1,11 @@
 module.exports = require('../utils/pluginRules').with('more', {
   'classbody-starts-with-newline': ['error', 'always'],
   'force-native-methods': 'warn',
+  'no-c-like-loops': 'warn',
   'no-duplicated-chains': 'warn',
   'no-numeric-endings-for-variables': 'error',
   'no-then': 'off',
+  'no-void-map': 'error',
   'no-window': 'warn',
+  'prefer-includes': 'off',
 });
--- ruleSets/promiseSet.js
+++ ruleSets/promiseSet.js
@@ -1,7 +1,11 @@
 module.exports = require('../utils/pluginRules').with('promise', {
   'always-return': 'off',
+  'avoid-new': 'off',
   'catch-or-return': 'warn',
+  'no-callback-in-promise': 'off',
   'no-native': 'off',
+  'no-nesting': 'warn',
+  'no-promise-in-callback': 'off',
   'no-return-wrap': 'error',
   'param-names': 'error',
   'prefer-await-to-callbacks': 'off',
