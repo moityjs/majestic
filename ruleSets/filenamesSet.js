e4aae971e17514e9a9b4eac200b0008bdbdb9972
Peet <peet@peet.me.uk>
2016-12-14 13:29:02 +0000

    filnames use camelCase unless export has name

--- ruleSets/filenamesSet.js
+++ ruleSets/filenamesSet.js
@@ -1,5 +1,5 @@
 module.exports = require('../utils/pluginRules').with('filenames', {
   'match-exported': 'error',
-  'match-regex': ['error', '^[a-z_]+$', true],
+  'match-regex': ['error', null, true],
   'no-index': 'off',
 });
